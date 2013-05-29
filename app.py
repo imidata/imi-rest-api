606f12ab13f022fa5229b15936d585dbe170577a
Jon Saints <jon@imidata.com>
2013-05-29 17:25:25 -0600

    slick way to handle autoreload when in development mode and ignore it in production

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+DEBUG = os.getenv('DEBUG',False)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG)
